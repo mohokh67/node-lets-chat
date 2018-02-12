cfd66faa2d5c6a4a2ea70a7219338c311cc92c26
MoHo <habibsandyfan@gmail.com>
2018-02-12 08:21:20 +0000

    Front end with bootstrap4

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname))
-var server = app.listen(3000, () => {
+var server = app.listen(3000, () => { // in browser: http://localhost:3000/
     console.log('Server is listening on port', server.address().port)
 });
