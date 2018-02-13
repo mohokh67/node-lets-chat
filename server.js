14fb7d65ee10d0a6d3fdd730e09ba805175fcfc0
MoHo <habibsandyfan@gmail.com>
2018-02-13 08:36:20 +0000

    Chat is working with get and post request

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(express.static(__dirname))
-var server = app.listen(3000, () => { // in browser: http://localhost:3000/
+app.use(bodyParser.json()) // Parse the body from get request
+app.use(bodyParser.urlencoded({extended: false})) // Parse the body of post request from jQuery
+
+
+var messages = [
+    { name: 'moho', message: 'Hi'},
+    { name: 'Habib', message: 'Hello'}
+];
+// Routes
+app.get('/messages', (request, response) =>{
+    response.send(messages)
+})
+
+app.post('/messages', (request, response) => {
+    messages.push(request.body);
+    response.sendStatus(200);
+})
+
+
+var server = app.listen(3002, () => { // in browser: http://localhost:3000/
     console.log('Server is listening on port', server.address().port)
 });
